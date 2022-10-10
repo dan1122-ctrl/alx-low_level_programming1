e1afebdca22302647d94d2e3fee8024462d82d6a
dan1122-ctrl <danieltekle88@gmail.com>
2022-10-10 10:49:02 +0300

    task 4-8 level.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Return: will be 1 here
+ */
+int main(void)
+{
+write (1,"and that piece of art is usifull\"-Dora korpar,2015-10-19\n",59);
+	return(1);
+}
---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -printing using puts
+ *
+ * Return :retun will be zero
+ */
+int main(void)
+{
+	puts("programing is like building a multiligual puzzele.");
+	return 0;
+}
